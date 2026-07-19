9d03bf25bff7a8164e497349d752999a528dc8e3
Michael S. Kenniston <git@michaelkenniston.com>
2026-07-18 23:39:47 -0400

    added verify-packages script

---
+++ tools/verify-packages.bash
@@ -0,0 +1,21 @@
+#! /usr/bin/env bash
+
+FAILS=0
+for CMD in racket python
+do
+  RES=`which $CMD`
+  if [ $? -ne 0 ]
+  then
+    echo "Tool \"$CMD\" is not found.  It must be installed."
+    FAILS=1
+  else
+    echo "found $RES"
+  fi
+done
+
+if [ $FAILS -eq 0 ]
+then
+  echo "All tools found, all required packages are present."
+fi
+exit $FAILS
+
