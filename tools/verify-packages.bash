a145c3cbe3fd9fa33b5bf7578d9cb07f144d7537
Michael S. Kenniston <git@michaelkenniston.com>
2026-07-23 08:48:55 -0400

    improvements to verify-packages script

--- tools/verify-packages.bash
+++ tools/verify-packages.bash
@@ -1,11 +1,42 @@
 #! /usr/bin/env bash
 
+# This script verifies that all commands are installed that are
+# required to run all the self-tests for all the target languages
+# supported by TEECLI.  You only need to run this script once,
+# manually (it does not auto-run), immediately after installing
+# TEECLI.
+# This script is just a hint to help you get started efficiently,
+# and it is safe to ignore any tools that you don't need -- for
+# example you can ignore the "dotnet" message if you aren't
+# testing any C# code.
+# Specific installation instructions for each tool are not included
+# here because those can vary depending on which Linux distribution
+# and version you are running.
+
+BASH_TOOLS=bash
+RACKET_TOOLS=racket
+PYTHON_TOOLS="python ruff"
+JAVASCRIPT_TOOLS=node
+JAVA_TOOLS="java javac"
+C_TOOLS=gcc
+CPLUSPLUS_TOOLS=cpp
+CSHARP_TOOLS=dotnet
+
 FAILS=0
-for CMD in shellcheck racket python ruff; do
-  if RES=$(command -v $CMD); then
+for TOOL in \
+	$BASH_TOOLS		\
+	$RACKET_TOOLS		\
+	$PYTHON_TOOLS		\
+	$JAVASCRIPT_TOOLS	\
+	$JAVA_TOOLS		\
+	$C_TOOLS		\
+	$CPLUSPLUS_TOOLS	\
+	$CSHARP_TOOLS		
+do
+  if RES=$(command -v "$TOOL"); then
     echo "found $RES"
   else
-    echo "Tool \"$CMD\" is not found.  It must be installed."
+    echo -e "\033[31mTool \"$TOOL\" is not found.  It must be installed.\033[0m"
     FAILS=1
   fi
 done
